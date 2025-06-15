b97129293c1cd50f1d10aee4491cbf97533c419c
xin-yu-666 <2144845402@qq.com>
2025-06-15 16:08:32 +0800

    first commit

---
+++ src/main/java/com/example/trainingserver/TrainingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trainingserver/TrainingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
